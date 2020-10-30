70dd9daea5311609addcaf249a67ea480c70003e
yo <yjeremij@ucsd.edu>
2020-10-29 18:02:05 -0700

    hi

--- run.py
+++ run.py
@@ -7,3 +7,9 @@ from spotlight.datasets.movielens import get_movielens_dataset
 
 dataset = get_movielens_dataset(variant='100K')
 print(dataset)
+
+from spotlight.cross_validation import random_train_test_split
+
+train, test = random_train_test_split(dataset, random_state=np.random.RandomState(42))
+
+print('Split into \n {} and \n {}.'.format(train, test))
